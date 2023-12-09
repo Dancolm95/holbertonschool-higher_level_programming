55cefaf5f64460cb4c732475b5c4ce92727159a3
dancolm95 <dancolm95@gmail.com>
2023-12-08 21:05:13 -0800

    script that computes the score average of all records in the table second_table of the database hbtn_0c_0

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- script computes the average of a given record
+-- computes the score average of all records in tthe table second_table of teh database hbtn_0c_0.
+SELECT AVG(score) AS average FROM second_table;
